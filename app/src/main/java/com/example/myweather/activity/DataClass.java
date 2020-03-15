ca1f33211b4696bca56654251804f4b3b93a5c15
VVaryag <VVaryag@githaub.com>
2020-03-15 20:31:35 +0200

    add spinner in ActivityShow

---
+++ app/src/main/java/com/example/myweather/activity/DataClass.java
@@ -0,0 +1,12 @@
+package com.example.myweather.activity;
+
+import java.util.ArrayList;
+
+public class DataClass {
+    public static final String TAG = "retrofit";
+    public static final String TAG1 = "spinner";
+    public static final String TAG2 = "ActivityShow";
+    public static String [] city = new String[]{"Kharkiv", "Kyiv", "Odesa"};
+
+
+}
---
+++ app/src/main/java/com/example/myweather/fragments/EnterInformation.java
@@ -0,0 +1,77 @@
+package com.example.myweather.activity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.myweather.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link EnterInformation#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EnterInformation extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public EnterInformation() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Enter_information.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EnterInformation newInstance(String param1, String param2) {
+        EnterInformation fragment = new EnterInformation();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Button btn = getView().findViewById(R.id.found_town);
+btn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+    }
+});
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_enter_information, container, false);
+    }
+}
