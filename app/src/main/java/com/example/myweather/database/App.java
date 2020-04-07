ed13f3a0cebebaf82337696d2c343a46a9a0b316
Oleg <46029146+VVaryag@users.noreply.github.com>
2020-04-07 19:58:37 +0300

    Update App.java

--- app/src/main/java/com/example/myweather/database/App.java
+++ app/src/main/java/com/example/myweather/database/App.java
@@ -12,9 +12,27 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        if(citiesDatabase == null){
         citiesDatabase = Room.
                 databaseBuilder(this, CitiesDatabase.class, "base_cities")
                 .allowMainThreadQueries().fallbackToDestructiveMigration().build();
+            CitiesDatabase cd = App.getInstance().getCitiesDatabase();
+            CityDao cityDao = cd.cityDao();
+
+            City town = new City("Kyiv");
+            City town2 = new City ("Odesa");
+            City town3 = new City("Kharkiv");
+            City town4 = new City ("Chernihiv");
+            City town5 = new City ("Kherson");
+            City town6 = new City ("Mykolaiv");
+            City town7 = new City ("Lviv");
+            cityDao.addCity(town3);
+            cityDao.addCity(town4);
+            cityDao.addCity(town5);
+            cityDao.addCity(town6);
+            cityDao.addCity(town7);
+            cityDao.addCity(town);
+            cityDao.addCity(town2);
     }
 
     public static  App getInstance(){
